3003223fe3d0555eb4be2dd72499e01a5b586175
zackery4b1n <121780030+zackery4b1n@users.noreply.github.com>
2023-07-07 11:29:13 +0800

    minor revesion

--- frontend/src/components/layouts/BaseHeader.vue
+++ frontend/src/components/layouts/BaseHeader.vue
@@ -30,28 +30,6 @@ import { toggleDark } from "~/composables";
     <RouterLink to="/StuInfo" class="custom-link">
       <el-menu-item index="6" class="menu-root-item">首页6</el-menu-item>
     </RouterLink>   
-    <!-- <el-menu-item index="2" class="menu-root-item">
-      <RouterLink to="/StuInfo" class="custom-link">用户列表</RouterLink>
-    </el-menu-item> -->
-    <!-- <el-sub-menu index="2" class="menu-root-item">
-      <template #title>用户列表</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu> -->
-    <!-- <el-menu-item index="3" class="menu-root-item" router-link to="/">月度积分</el-menu-item>
-    <el-menu-item index="4" class="menu-root-item">codeforces</el-menu-item>
-
-    <el-menu-item index="5" class="menu-root-item">atcoder</el-menu-item>
-    <el-menu-item index="6" class="menu-root-item">图表</el-menu-item>
-    <el-menu-item index="7" class="menu-root-item">智能训练</el-menu-item>
-    <el-menu-item index="8" class="menu-root-item">更新信息</el-menu-item> -->
   </el-menu>
 </template>
 
